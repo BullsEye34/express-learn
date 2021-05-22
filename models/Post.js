46b3d360bd36ed2357504f43265184a26fed2734
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-22 21:51:30 +0530

    Schema

--- models/Post.js
+++ models/Post.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 
+const postsSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type:String, 
+        required:true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+})
