d14f8add70cae362b55a93d3c51ef4f4cef9912e
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-22 21:40:59 +0530

    Connect through ENV

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 /// Import Requirements
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv/config')
 
 /// Initialise Server
 const app = express();
 
 /// Connect to DB
-mongoose.connect('mongodb+srv://vamshi:vamshiVIJ15@cluster0.wopx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, ()=>console.log("Connected To DB"));
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, ()=>console.log("Connected To DB"));
 
 /// Listen on port 3000
 app.listen(3000);
