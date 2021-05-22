78b55ea034c27cc6cf34ff2a11f71d9b1592e0af
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-22 21:46:49 +0530

    Organise

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv/config')
 
+
+/// Import Routes
+const postsRoute = require('./routes/post');
+
+
 /// Initialise Server
 const app = express();
+
+/// Routes
+app.use('/posts', postsRoute);
+
 
 /// Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, ()=>console.log("Connected To DB"));
