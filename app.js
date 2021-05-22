98d0e6c117bd3dcbacf61e01b6ca93da26a3c28d
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-22 21:39:34 +0530

    Connect to DB

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+/// Import Requirements
 const express = require('express');
 const mongoose = require('mongoose');
+
+/// Initialise Server
 const app = express();
 
-app.get("/",(req,res)=>res.send({"Hi":"Heyyo"}))
+/// Connect to DB
+mongoose.connect('mongodb+srv://vamshi:vamshiVIJ15@cluster0.wopx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, ()=>console.log("Connected To DB"));
 
+/// Listen on port 3000
 app.listen(3000);
