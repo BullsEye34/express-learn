ba36aa486f7acdfc448794dd8599c2f1ff99a6b8
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-22 21:57:35 +0530

    Route to Post

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const Post = require('../models/Post');
 const router = express.Router();
 
 router.get('/', (req,res)=>{
     res.send('We are on Posts');
+});
+
+router.post('/', (req,res)=>{
+    console.log(req.body);
+    
 })
 
 module.exports=router;
