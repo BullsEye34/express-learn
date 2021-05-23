6df811b2ef60c4b60eff4fd0c56ec3e72e589f44
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-23 20:41:43 +0530

    Validation

--- routes/validation.js
+++ routes/validation.js
@@ -1,14 +1,14 @@
 const Joi = require('joi');
 
 
-const registerValidation = ()=> {
+const registerValidation = data=> {
     const schema = Joi.object({
         name: Joi.string().min(6).max(25).required(),
         email: Joi.string().min(6).email(),
         password: Joi.string().min(6),
     });
     
-    
+    return schema.validate(data);
 
     
 }
