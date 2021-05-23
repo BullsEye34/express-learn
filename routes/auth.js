003c99c604530ac0d0b88e35c0f009b5cd4557a7
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-23 20:21:31 +0530

    Add Userr

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,23 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 
-router.post('/register',(req,res)=>{
-    console.log("Register")
+router.post('/register', async (req,res)=>{
+    const user = new User({
+        name: req.body.name,
+        password: req.body.password,
+        phone: req.body.phone,
+        img: req.body.img,
+    });
+    try{
+        const newUser = await user.save();
+        res.json({created: newUser});
+
+    }catch(err){
+        res.json({
+            err: true,
+            message: err
+        }).status(400);
+    }
     res.json({page:"Register"})
 })
 
