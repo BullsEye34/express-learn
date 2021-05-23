da456ff4c8ac19e2dc12181a47dc87ed2d9df619
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-23 20:07:16 +0530

    Define Schema

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required:true,
+    },
+    phone:{
+        type: Number,
+        
+    },
+    img: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/previews/001/196/558/original/person-png.png",
+
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    }
+});
